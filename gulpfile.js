387f234433fa51e81db0075c3211313ff9f429dc
Hannes Lilljequist <hannes@sthlmconnection.se>
2016-03-23 23:32:46 +0100

    Added Gulp, added a YAML data file and a template, replaced ruby sass with libsass.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var yaml = require('js-yaml');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+var fs = require('fs');
+var moment = require('moment');
+var sass = require('gulp-sass');
+var compass = require('compass-importer');
+
+moment.locale('sv');
+
+gulp.task('template', function () {
+  var templateData = yaml.safeLoad(fs.readFileSync('src/data.yml', 'utf-8'));
+
+  var processEvent = function (event) {
+    event.dates = event.dates.map(function (date) {
+      date.formatted = moment(date.date).format('dddd D MMMM');
+      return date;
+    });
+    return event;
+  };
+
+  templateData.walks = templateData.walks.map(processEvent);
+  templateData.excursions = templateData.excursions.map(processEvent);
+
+  return gulp.src('src/index.hbs')
+    .pipe(handlebars(templateData))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({ importer: compass }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['template', 'sass']);
